744ae7ea831af565752632e0a02c4164d8097ba0
ARTHUR VON ROHR MONTEIRO <1137370@atitus.local>
2026-05-04 21:07:46 -0300

    Diagramas e pedaços de models

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,2 +1,9 @@
 from django.db import models
-# Create your models here.
+
+class User(models.AbstractUser):
+    pass
+
+class Group(models.Model):
+    creator = models.ForeignKey(User,on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
