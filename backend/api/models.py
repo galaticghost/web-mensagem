db326d346ec5e77c1891db4e2714d6a4da47f7d6
Arthur Suse <galaticghost@gmail.com>
2026-05-08 17:43:48 -0300

    Atualização nos modelos WIP

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,3 +11,21 @@ class Group(models.Model):
     creator = models.ForeignKey(User,on_delete=models.CASCADE)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    members = models.ManyToManyField(User,through="GroupMember",related_name="groups")
+
+class GroupMember(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    group = models.ForeignKey(Group,on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+
+class Chat(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PrivateChat(models.Model):
+    chat = models.ForeignKey(Chat,on_delete=models.CASCADE)
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat,on_delete=models.CASCADE)
+    sender = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=500)
